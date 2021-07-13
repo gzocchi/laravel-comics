d99ef2bf08e6308144cb6f37d2f667c589df4482
gzocchi <z.giorgio.z@gmail.com>
2021-07-13 17:11:06 +0200

    correzione header e footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,133 +1,133 @@
 <footer>
-   <section class="footer_link">
-     <div class="container">
-       <div class="list">
-         <div class="list_block">
-           <h3>dc comics</h3>
-           <ul>
-             <li>
-               <a href="#">Characters</a>
-             </li>
-             <li>
-               <a href="#">Comics</a>
-             </li>
-             <li>
-               <a href="#">Movies</a>
-             </li>
-             <li>
-               <a href="#">TV</a>
-             </li>
-             <li>
-               <a href="#">Games</a>
-             </li>
-             <li>
-               <a href="#">Video</a>
-             </li>
-             <li>
-               <a href="#">News</a>
-             </li>
-           </ul>
-         </div>
+  <section class="footer_link">
+    <div class="container">
+      <div class="list">
+        <div class="list_block">
+          <h3>dc comics</h3>
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Video</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+          </ul>
+        </div>
 
-         <div class="list_block">
-           <h3>Shop</h3>
-           <ul>
-             <li>
-               <a href="#">Shop DC</a>
-             </li>
-             <li>
-               <a href="#">Shop DC Collectibles</a>
-             </li>
-           </ul>
-         </div>
+        <div class="list_block">
+          <h3>Shop</h3>
+          <ul>
+            <li>
+              <a href="#">Shop DC</a>
+            </li>
+            <li>
+              <a href="#">Shop DC Collectibles</a>
+            </li>
+          </ul>
+        </div>
 
-         <div class="list_block">
-           <h3>dc</h3>
-           <ul>
-             <li>
-               <a href="#">Terms Of Use</a>
-             </li>
-             <li>
-               <a href="#">Privacy policy (New)</a>
-             </li>
-             <li>
-               <a href="#">Ad Choices</a>
-             </li>
-             <li>
-               <a href="#">Advertising</a>
-             </li>
-             <li>
-               <a href="#">Jobs</a>
-             </li>
-             <li>
-               <a href="#">Subscriptions</a>
-             </li>
-             <li>
-               <a href="#">Talent Workshops</a>
-             </li>
-             <li>
-               <a href="#">CPSC Certificates</a>
-             </li>
-             <li>
-               <a href="#">Ratings</a>
-             </li>
-             <li>
-               <a href="#">Shop Help</a>
-             </li>
-             <li>
-               <a href="#">Contact Us</a>
-             </li>
-           </ul>
-         </div>
+        <div class="list_block">
+          <h3>dc</h3>
+          <ul>
+            <li>
+              <a href="#">Terms Of Use</a>
+            </li>
+            <li>
+              <a href="#">Privacy policy (New)</a>
+            </li>
+            <li>
+              <a href="#">Ad Choices</a>
+            </li>
+            <li>
+              <a href="#">Advertising</a>
+            </li>
+            <li>
+              <a href="#">Jobs</a>
+            </li>
+            <li>
+              <a href="#">Subscriptions</a>
+            </li>
+            <li>
+              <a href="#">Talent Workshops</a>
+            </li>
+            <li>
+              <a href="#">CPSC Certificates</a>
+            </li>
+            <li>
+              <a href="#">Ratings</a>
+            </li>
+            <li>
+              <a href="#">Shop Help</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </div>
 
-         <div class="list_block">
-           <h3>Sites</h3>
-           <ul>
-             <li>
-               <a href="#">DC</a>
-             </li>
-             <li>
-               <a href="#">MAD Magazine</a>
-             </li>
-             <li>
-               <a href="#">DC Kids</a>
-             </li>
-             <li>
-               <a href="#">DC Universe</a>
-             </li>
-             <li>
-               <a href="#">DC Power Visa</a>
-             </li>
-           </ul>
-         </div>
-       </div>
-     </div>
-   </section>
+        <div class="list_block">
+          <h3>Sites</h3>
+          <ul>
+            <li>
+              <a href="#">DC</a>
+            </li>
+            <li>
+              <a href="#">MAD Magazine</a>
+            </li>
+            <li>
+              <a href="#">DC Kids</a>
+            </li>
+            <li>
+              <a href="#">DC Universe</a>
+            </li>
+            <li>
+              <a href="#">DC Power Visa</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 
-   <section class="follow">
-     <div class="container">
-       <div class="sign_up">
-         <a href="#">sign-up now!</a>
-       </div>
+  <section class="follow">
+    <div class="container">
+      <div class="sign_up">
+        <a href="#">sign-up now!</a>
+      </div>
 
-       <div class="social">
-         <a href="#">follow us</a>
-         <a href="#">
-           <img src="{{ asset('img/footer-facebook.png') }}" alt="facebook" />
-         </a>
-         <a href="#">
-           <img src="{{ asset('img/footer-twitter.png') }}" alt="twitter" />
-         </a>
-         <a href="#">
-           <img src="{{ asset('img/footer-youtube.png') }}" alt="youtube" />
-         </a>
-         <a href="#">
-           <img src="{{ asset('img/footer-pinterest.png') }}" alt="pinterest" />
-         </a>
-         <a href="#">
-           <img src="{{ asset('img/footer-periscope.png') }}" alt="periscope" />
-         </a>
-       </div>
-     </div>
-   </section>
+      <div class="social">
+        <a href="#">follow us</a>
+        <a href="#">
+          <img src="{{ asset('img/footer-facebook.png') }}" alt="facebook" />
+        </a>
+        <a href="#">
+          <img src="{{ asset('img/footer-twitter.png') }}" alt="twitter" />
+        </a>
+        <a href="#">
+          <img src="{{ asset('img/footer-youtube.png') }}" alt="youtube" />
+        </a>
+        <a href="#">
+          <img src="{{ asset('img/footer-pinterest.png') }}" alt="pinterest" />
+        </a>
+        <a href="#">
+          <img src="{{ asset('img/footer-periscope.png') }}" alt="periscope" />
+        </a>
+      </div>
+    </div>
+  </section>
  </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,16 @@
 <header>
+  <div class="top">
     <div class="container">
+      <a href="#" target="_blank">
+        DC Power<span>&#8480;</span> Visa<span>&#174;</span>
+      </a>
+      <a href="#" target="_blank">
+        Additional DC Sites
+        <i class="fas fa-caret-down"></i>
+      </a>
+    </div>
+  </div>
+    <div class="container bottom">
       <a class="logo" href="#">
         <img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo" />
       </a>
@@ -52,6 +63,7 @@
         <li>
             <a href="#">
               Shop
+              <i class="fas fa-caret-down"></i>
             </a>
         </li>
       </ul>
